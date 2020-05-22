59d4adc984ce913a1f215c34c069850afc9c3a0d
Laith Harb <harblaith77@gmail.com>
2020-05-22 14:21:25 -0400

    added first test

--- HeadlessTesting/test.test.js
+++ HeadlessTesting/test.test.js
@@ -1,1 +1,22 @@
-hi
+const puppeteer = require("puppeteer");
+
+jest.setTimeout(30000)
+
+let browser, page
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        headless: false
+    })
+    page = await browser.newPage()
+    await page.goto("localhost:3000")
+})
+
+afterEach(async () => {
+    await browser.close()
+})
+
+test("Launched correctly", async () => {
+    const html = await page.$eval(".Header__heading", e => e.innerHTML)
+    expect(html).toEqual("Follow your passions. Achieve your dreams.")
+})
